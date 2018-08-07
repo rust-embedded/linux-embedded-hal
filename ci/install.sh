12e9d7aeb2d6ad241b59e32bf4339e0656a6f724
Jorge Aparicio <jorge@japaric.io>
2018-08-06 23:18:16 -0500

    update ci

--- ci/install.sh
+++ ci/install.sh
@@ -1,18 +1,9 @@
-set -ex
+set -euxo pipefail
 
 main() {
-    # This fetches latest stable release of Cross
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0.1.0-9.]+$' \
-                       | sort --version-sort \
-                       | tail -n1)
-
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/cross \
-           --tag $tag
+    if [ $TARGET != x86_64-unknown-linux-gnu ]; then
+        rustup target add $TARGET
+    fi
 }
 
 main
